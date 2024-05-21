74b1d59117ce71684b4f538eb3fa2a411603ea49
Mathias <148339583+Mattmoimeme@users.noreply.github.com>
2024-05-21 16:11:31 +0200

    modif header

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Accueil</a>
+    <a class="navbar-brand" href="index.php">Accueil</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,11 +10,15 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Derniers articles</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="create_article.php">Créer son article</a>
+        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Catégories
           </a>
           <ul class="dropdown-menu">
+            <!-- A voir comment rediriger vers les articles spécifiques à chaque catégorie -->
             <li><a class="dropdown-item" href="#">Technologie</a></li>
             <li><a class="dropdown-item" href="#">Santé</a></li>
             <li><a class="dropdown-item" href="#">Science</a></li>
