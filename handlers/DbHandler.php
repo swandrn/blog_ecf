177260d5d7d8f5708a1890f7d467c7da707f922f
swandrn <159421702+swandrn@users.noreply.github.com>
2024-05-21 12:16:28 +0200

    Creation fonction pour select un article a partir de son id + meilleur commentaires

--- handlers/DbHandler.php
+++ handlers/DbHandler.php
@@ -16,19 +16,26 @@ class DbHandler
         }
     }
 
-    //Insert article data in DB
-    function insertArticle($title, $content, $author, $category)
+    /**
+     * Insert article data in DB
+     * @param string $title
+     * @param string $content
+     * @param string $author
+     * @param int $category
+     */
+    
+    function insertArticle($title, $content, $author, $categoryId)
     {
         $currentDate = date("Y-m-d");
         try {
             $conn = $this->openDbConnection();
             $stmt = $conn->prepare("INSERT INTO articles (titre, contenu, auteur, dateCreation, categorieId)
-            VALUES (:title, :content, :author, :currentDate, :category)");
+            VALUES (:title, :content, :author, :currentDate, :categoryId)");
             $stmt->bindParam(':title', $title, PDO::PARAM_STR);
             $stmt->bindParam(':content', $content, PDO::PARAM_STR);
             $stmt->bindParam(':author', $author, PDO::PARAM_STR);
             $stmt->bindParam(':currentDate', $currentDate, PDO::PARAM_STR);
-            $stmt->bindParam(':category', $category, PDO::PARAM_INT);
+            $stmt->bindParam(':categoryId', $categoryId, PDO::PARAM_INT);
             $stmt->execute();
             $conn = null;
         } catch (PDOException $e) {
@@ -36,13 +43,34 @@ class DbHandler
         }
     }
 
-    //Get all the existing articles in DB
+    /**
+     * Get all the existing articles in DB
+     * @return array array of all articles
+     */
     function selectAllArticles(){
         try{
             $conn = $this->openDbConnection();
             $stmt = $conn->prepare("SELECT titre, contenu, auteur, dateCreation, categorieId FROM articles");
             $stmt->execute();
             $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $res;
+        } catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+    /**
+     * Select the article of a given ID
+     * @param int $id id of article to select
+     * @return array array of key value where key is column name
+     */
+    function selectArticle($id){
+        try{
+            $conn = $this->openDbConnection();
+            $stmt = $conn->prepare("SELECT titre, contenu, auteur, dateCreation, categorieId FROM articles WHERE id_article=:id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $res = $stmt->fetch(PDO::FETCH_ASSOC);
             return $res;
         } catch(PDOException $e){
             echo $e->getMessage();
