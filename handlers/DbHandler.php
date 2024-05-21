13f0241dadc72d952000d20697532299bb8d06cc
swandrn <159421702+swandrn@users.noreply.github.com>
2024-05-21 11:49:44 +0200

    Creation fonction insert article

--- handlers/DbHandler.php
+++ handlers/DbHandler.php
@@ -1,16 +1,36 @@
 <?php
-class DbHandler{
-    private $servername = 'localhost';
+class DbHandler
+{
     private $username = 'root';
     private $password = '';
 
     //Connection
-    function openDbConnection(){
-        try{
-            $conn = new PDO("mysql:host=$this->servername;dbname=blogecf", $this->username, $this->password);
+    function openDbConnection()
+    {
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=blogecf", $this->username, $this->password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
-        } catch(PDOException $e){
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    function insertArticle($title, $content, $author, $category)
+    {
+        $currentDate = date("Y-m-d");
+        try {
+            $conn = $this->openDbConnection();
+            $stmt = $conn->prepare("INSERT INTO articles (titre, contenu, auteur, dateCreation, categorieId)
+            VALUES (:title, :content, :author, :currentDate, :category)");
+            $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+            $stmt->bindParam(':content', $content, PDO::PARAM_STR);
+            $stmt->bindParam(':author', $author, PDO::PARAM_STR);
+            $stmt->bindParam(':currentDate', $currentDate, PDO::PARAM_STR);
+            $stmt->bindParam(':category', $category, PDO::PARAM_INT);
+            $stmt->execute();
+            $conn = null;
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
