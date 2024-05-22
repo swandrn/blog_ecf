d4ce5aa268ac3b92eb4990bc75e14954a8f97685
swandrn <159421702+swandrn@users.noreply.github.com>
2024-05-22 22:56:20 +0200

    Insertion d'un nouveau commentaire dans la BDD

---
+++ handlers/add_comment_handler.php
@@ -0,0 +1,20 @@
+<?php
+require_once './DbHandler.php';
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$db = new DbHandler();
+
+if (!empty($_POST)){
+    $author = $_SESSION['username'];
+    $articleId = (int)$_POST['articleId'];
+    $userId = $db->selectUserId($author);
+    $content = $_POST['comment'];
+
+    $db->insertComment($author, $articleId, $userId, $content);
+
+    header('Location: ../details_article.php?id=' . $articleId);
+    exit;
+}
+?>
