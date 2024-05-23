56dffa2189b15f21aac2645ba92690490c8d4f39
Mathias <148339583+Mattmoimeme@users.noreply.github.com>
2024-05-23 13:01:30 +0200

    redirection edit_article modif utilisateur

--- edit_article.php
+++ edit_article.php
@@ -1,25 +1,68 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
+
+<?php require_once './handlers/article_data_handler.php'; ?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Détails de l'article</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./css/edit_article.css">
+    <link href="css/style_formulaire.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/header.css">
-    <title>Modifier un article</title>
 </head>
 
 <body>
-    
+
     <?php require 'header.php' ?>
-    <main>
-        <div class="article">
-            <h2>Article 1 (extrait)</h2>
-            <p>Appuyer ici pour editer votre article</p>
+    <div class="container mt-5">
+
+        <!-- Bloc Article -->
+        <div class="card mb-4">
+            <div class="card-header">
+                Article : <?= $article['titre']; ?>
+            </div>
+            <div class="card-body">
+                <p><?= $article['contenu']; ?></p>
+            </div>
         </div>
-    </main>
+
+        <!-- Derniers commentaires -->
+        <div class="card mb-4">
+            <div class="card-header">
+                Derniers commentaires :
+            </div>
+            <?php foreach ($comments as $comment) : ?>
+                <div class="card-body">
+                    <p class="author"><?= $comment['auteur'] ?></p>
+                    <p class="content"><?= $comment['contenu'] ?></p>
+                    <p class="creation-date"><?= $comment['date_creation'] ?></p>
+                    <?php if (isset($_SESSION['username'])) : ?>
+                        <?php if ($comment['auteur'] == $_SESSION['username'] || $article['auteur'] == $_SESSION['username']) : ?>
+                            <form action="./handlers/comment_handler.php" method="POST">
+                                <input type="hidden" name="commentId" value="<?= $comment['id_commentaire'] ?>">
+                                <input type="hidden" name="articleId" value="<?php if (!empty($_GET)) {echo htmlspecialchars($_GET['id']);} ?>">
+                                <button type="submit" class="btn btn-primary" id="delete">Supprimer</button>
+                            </form>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+        <!-- Bloc Laisser un commentaire -->
+        <div class="card mb-4">
+            <div class="card-header d-flex justify-content-between">
+                Actions :
+                <div>
+                    <button class="btn btn-primary" id="save">Modifier</button>
+                    <button class="btn btn-danger" id="supprPubli">Supprimer la publication et les commentaires</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php require 'footer.php' ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
