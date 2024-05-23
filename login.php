4042abd0cadf4f8927bcebb81ea2dc10c5af8f18
AslanPiab <p-aslan@hotmail.fr>
2024-05-23 09:51:06 +0200

    changement style register et login, ajout de la nav

--- login.php
+++ login.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/creator_article_style.css" rel="stylesheet">
+    <link href="css/style_formulaire.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
@@ -19,21 +20,23 @@
         </nav>
     </header> -->
     <?php require 'header.php' ?>
-    <div class="form-container">
-        <h1 class="text-center mb-4">Connexion</h1>
-        <form action="handlers/traitement_login.php" method="post">
-            <div class="form-group">
-                <label for="email">Adresse e-mail :</label>
-                <input type="email" class="form-control" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Mot de passe :</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
-            <div class="text-center">
-                <button type="submit" class="btn btn-success" id="login">Se connecter</button>
-            </div>
-        </form>
+    <div class="page-container">
+        <div class="form-container">
+            <h1 class="text-center mb-4">Connexion</h1>
+            <form action="handlers/traitement_login.php" method="post">
+                <div class="form-group">
+                    <label for="email">Adresse e-mail :</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Mot de passe :</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-success" id="login">Se connecter</button>
+                </div>
+            </form>
+        </div>
     </div>
     <?php require 'footer.php' ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
--- register.php
+++ register.php
@@ -7,45 +7,41 @@
     <title>Inscription</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style_formulaire.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="index.php">Liste des articles</a></li>
-                <li><a href="edit_article.php">Editer un article</a></li>
-            </ul>
-        </nav>
-    </header>
+
+    <?php require 'header.php' ?>
+
     <div class=page-container>
-    <div class="form-container">
-        <h1 class="text-center mb-4">Inscription</h1>
-        <form action="handlers/traitement_inscription.php" method="post">
-            <div class="form-group">
-                <label for="nom">Nom :</label>
-                <input type="text" class="form-control" id="nom" name="nom" required>
-            </div>
-            <div class="form-group">
-                <label for="prenom">Prénom :</label>
-                <input type="text" class="form-control" id="prenom" name="prenom" required>
-            </div>
-            <div class="form-group">
-                <label for="pseudo">Pseudo :</label>
-                <input type="text" class="form-control" id="pseudo" name="pseudo" required>
-            </div>
-            <div class="form-group">
-                <label for="email">Email :</label>
-                <input type="email" class="form-control" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label for="pass">Mot de passe :</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
-            <div class="text-center">
-                <button type="submit" class="btn btn-primary" value ="M'inscrire" name="ok">S'inscrire</button>
-        </form>
-    </div>
+        <div class="form-container">
+            <h1 class="text-center mb-4">Inscription</h1>
+            <form action="handlers/traitement_inscription.php" method="post">
+                <div class="form-group">
+                    <label for="nom">Nom :</label>
+                    <input type="text" class="form-control" id="nom" name="nom" required>
+                </div>
+                <div class="form-group">
+                    <label for="prenom">Prénom :</label>
+                    <input type="text" class="form-control" id="prenom" name="prenom" required>
+                </div>
+                <div class="form-group">
+                    <label for="pseudo">Pseudo :</label>
+                    <input type="text" class="form-control" id="pseudo" name="pseudo" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email :</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="pass">Mot de passe :</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary" value="M'inscrire" name="ok">S'inscrire</button>
+            </form>
+        </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
