6e5ab024ca349b9c683f6b544871074757aa83d2
swandrn <159421702+swandrn@users.noreply.github.com>
2024-05-23 19:59:37 +0200

    Affiche le nom de la categorie dans la card article

--- index.php
+++ index.php
@@ -20,32 +20,33 @@
     <h1>Accueil</h1>
     <main class="container mt-5">
         <div class="row">
-            <?php foreach($articles as $article): ?>
-            <div class="col-md-4 mb-4">
-                <div class="card" data-article-id="<?= htmlspecialchars($article['id_article']); ?>">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= htmlspecialchars($article['titre']); ?></h5>
-                        <?php
-                        // Longueur max du nombre de caractères (à voir combien on voudra)
-                        $maxLength = 100;
-                        $content = htmlspecialchars($article['contenu']);
-                        //mb_strlen : prend en compte tous les caractère
-                        if (mb_strlen($content) > $maxLength) {
-                            // mb_substre lit une sous chaîne en partant de 0 jusqu'au max caractères
-                            $content = mb_substr($content, 0, $maxLength) . '...';
-                        }
-                        ?>
-                        <!-- nl2br insère un retour à la ligne HTML à chaque nouvelle ligne -->
-                        <p class="card-text"><?= nl2br($content); ?></p>
-                        <!-- Au clic sur lire la suite, redirige vers l'article -->
-                        <a href="details_article.php?id=<?= htmlspecialchars($article['id_article']); ?>" class="btn btn-primary neon-text">Lire la suite</a>
+            <?php foreach ($articles as $article) : ?>
+                <div class="col-md-4 mb-4">
+                    <div class="card" data-article-id="<?= htmlspecialchars($article['id_article']); ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= htmlspecialchars($article['titre']); ?></h5>
+                            <?php
+                            // Longueur max du nombre de caractères (à voir combien on voudra)
+                            $maxLength = 100;
+                            $content = htmlspecialchars($article['contenu']);
+                            //mb_strlen : prend en compte tous les caractère
+                            if (mb_strlen($content) > $maxLength) {
+                                // mb_substre lit une sous chaîne en partant de 0 jusqu'au max caractères
+                                $content = mb_substr($content, 0, $maxLength) . '...';
+                            }
+                            ?>
+                            <!-- nl2br insère un retour à la ligne HTML à chaque nouvelle ligne -->
+                            <p class="card-text"><?= nl2br($content); ?></p>
+                            <!-- Au clic sur lire la suite, redirige vers l'article -->
+                            <a href="details_article.php?id=<?= htmlspecialchars($article['id_article']); ?>" class="btn btn-primary neon-text">Lire la suite</a>
+                            <h5 class="card-title"><?= htmlspecialchars($db->getCategoryName($article['categorie_id'])); ?></h5>
+                        </div>
                     </div>
                 </div>
-            </div>
             <?php endforeach; ?>
         </div>
     </main>
-    
+
     <?php require 'footer.php'; ?>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
