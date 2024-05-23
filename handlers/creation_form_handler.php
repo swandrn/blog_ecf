4400f6433a78069203283ee0732d6e671c13e126
swandrn <159421702+swandrn@users.noreply.github.com>
2024-05-23 12:11:05 +0200

    Corriger erreur ou l'id utilisateur etait hard coded

--- handlers/creation_form_handler.php
+++ handlers/creation_form_handler.php
@@ -1,13 +1,16 @@
 <?php
 require_once './DbHandler.php';
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$db = new DbHandler();
 
 $title = $_POST['title'] ?? null;
 $content = $_POST['content'] ?? null;
-$categoryId = $_POST['categoryId'] ?? null;
-$author = $_SESSION['username'] ?? 'tata';
-$userId = 1;
-
-$db = new DbHandler();
+$categoryId = (int)$_POST['categoryId'] ?? null;
+$author = $_SESSION['username'];
+$userId = $db->selectUserId($author);
 
 $db->insertArticle($userId, $categoryId, $title, $content, $author);
 
