04f3470e9c4a37fd4686d09621d9c03852e18c95
Mathias <148339583+Mattmoimeme@users.noreply.github.com>
2024-05-24 10:19:10 +0200

    spammer blog

--- header.php
+++ header.php
@@ -13,22 +13,7 @@ if (session_status() === PHP_SESSION_NONE) {
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <!-- <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Derniers articles</a>
-        </li> -->
-          <!-- <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Catégories
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Technologie</a></li>
-              <li><a class="dropdown-item" href="#">Santé</a></li>
-              <li><a class="dropdown-item" href="#">Science</a></li>
-              <li><a class="dropdown-item" href="#">Education</a></li>
-              <li><a class="dropdown-item" href="#">Voyage</a></li>
-            </ul>
-          </li> -->
-          <!-- Si pas connecté => connexion si connecté => Déconnexion + accès à la page Mes articles -->
+          
           <?php if (empty($_SESSION['username'])) : ?>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="login.php">Connexion</a>
