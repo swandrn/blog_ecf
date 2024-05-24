b6a463bd04984e568bf980b464372f5c2e7fe619
Mathias <148339583+Mattmoimeme@users.noreply.github.com>
2024-05-24 14:32:45 +0200

    liens en in-line footer

--- footer.php
+++ footer.php
@@ -1,13 +1,16 @@
 <footer>
+
+    <div class="footer-container">
         <p>&copy; 2024</p>
-    <div class ="footer-container">
-        <!-- class list-unstyled de bootstrap pour enlever les puces -->
-        <ul class ="list-unstyled">
-            <li><a href="https://github.com/swandrn/blog_ecf" target="_blank"><i class="fab fa-git-alt"></i> Projet : Blog_Ecf4_Groupe1</a></li>
-            <li><a href="https://github.com/swandrn" target="_blank"><i class="fab fa-github"></i> Swan / @swandrn</a></li>
-            <li><a href="https://github.com/Mattmoimeme" target="_blank"><i class="fab fa-github"></i> Mathias / @Mattmoimeme</a></li>
-            <li><a href="https://github.com/AslanPiab" target="_blank"><i class="fab fa-github"></i> Pierre / @AslanPiab</a></li>
-            <li><a href="https://github.com/AntoninVon" target="_blank"><i class="fab fa-github"></i> Antonin / @AntoninVon</a></li>
+    
+               <!-- class list-unstyled de bootstrap pour enlever les puces -->
+        <ul class="list-unstyled">
+                <!-- class d-inline bootsrap pour mettre en ligne -->
+            <li class="d-inline mx-2"><a href="https://github.com/swandrn/blog_ecf" target="_blank"><i class="fab fa-git-alt"></i> Projet : Blog_Ecf4_Groupe1</a></li>
+            <li class="d-inline mx-2"><a href="https://github.com/swandrn" target="_blank"><i class="fab fa-github"></i> Swan / @swandrn</a></li>
+            <li class="d-inline mx-2"><a href="https://github.com/Mattmoimeme" target="_blank"><i class="fab fa-github"></i> Mathias / @Mattmoimeme</a></li>
+            <li class="d-inline mx-2"><a href="https://github.com/AslanPiab" target="_blank"><i class="fab fa-github"></i> Pierre / @AslanPiab</a></li>
+            <li class="d-inline mx-2"><a href="https://github.com/AntoninVon" target="_blank"><i class="fab fa-github"></i> Antonin / @AntoninVon</a></li>
         </ul>
     </div>
 </footer>
